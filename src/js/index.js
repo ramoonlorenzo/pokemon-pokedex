46c547b43fd94f27e1eba3ce004a04f93b03b69d
Ramon Lorenzo <69395268+ramoonlorenzo@users.noreply.github.com>
2023-06-19 15:32:45 -0300

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+const body = document.querySelector("body");
+
+const buttonThemeAlt = document.getElementById("button-theme-alt");
+
+const buttonImageAlt = document.querySelector(".button-image")
+
+buttonThemeAlt.addEventListener("click", () => {
+    const isDarkThemeActive = body.classList.contains("dark-theme");
+
+    body.classList.toggle("dark-theme");
+
+    if (isDarkThemeActive) {
+        buttonImageAlt.setAttribute("src", "./src/images/sun.png");
+    } else {
+        buttonImageAlt.setAttribute("src", "./src/images/moon.png");
+    }
+});
