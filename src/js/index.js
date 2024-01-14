c4e1f22de164d42c777245c397fa14a299369164
Ramon Lorenzo <69395268+ramoonlorenzo@users.noreply.github.com>
2024-01-14 13:53:18 -0300

    Update code formatter

--- src/js/index.js
+++ src/js/index.js
@@ -2,16 +2,16 @@ const body = document.querySelector("body");
 
 const buttonThemeAlt = document.getElementById("button-theme-alt");
 
-const buttonImageAlt = document.querySelector(".button-image")
+const buttonImageAlt = document.querySelector(".button-image");
 
 buttonThemeAlt.addEventListener("click", () => {
-    const isDarkThemeActive = body.classList.contains("dark-theme");
+  const isDarkThemeActive = body.classList.contains("dark-theme");
 
-    body.classList.toggle("dark-theme");
+  body.classList.toggle("dark-theme");
 
-    if (isDarkThemeActive) {
-        buttonImageAlt.setAttribute("src", "./src/images/sun.png");
-    } else {
-        buttonImageAlt.setAttribute("src", "./src/images/moon.png");
-    }
+  if (isDarkThemeActive) {
+    buttonImageAlt.setAttribute("src", "./src/images/sun.png");
+  } else {
+    buttonImageAlt.setAttribute("src", "./src/images/moon.png");
+  }
 });
